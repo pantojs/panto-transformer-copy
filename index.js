bf2bf4a5abd3265ad3d157cdd3e0a5f23b4f9f81
yinyong02 <yinyong02@baidu.com>
2016-07-31 23:20:17 +0800

    fixed #1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  * 2016-07-22[14:09:12]:revised
  *
  * @author yanni4night@gmail.com
- * @version 0.1.1
+ * @version 0.1.3
  * @since 0.1.0
  */
 'use strict';
@@ -19,7 +19,9 @@ class CopyTransformer extends Transformer {
 
         return panto.file.copy(file.filename, path.join(destdir,file.filename));
     }
-
+    isCacheable() {
+        return false;
+    }
 }
 
 module.exports = CopyTransformer;
